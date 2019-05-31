32f83938866998b02606da3947858ef77ab6662e
Becheru888 <becheru93@gmail.com>
2019-05-31 08:43:18 +0100

    List of cards with all characters added

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,12 +5,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
+      // films: [],
+      // homeworld:[],
+      // species:[],
+      // starships:[],
+      // vehicles:[],
     };
+
   }
 
   componentDidMount() {
     this.getCharacters('https://swapi.co/api/people/');
+    this.getFilms('https://swapi.co/api/films/')
+    // this.getHomeWorld('https://swapi.co/api/planets/')
+    // this.getSpecies('https://swapi.co/api/species/')
+    // this.getStarShips('https://swapi.co/api/starships/')
   }
 
   getCharacters = URL => {
@@ -29,10 +39,83 @@ class App extends Component {
       });
   };
 
+  getFilms = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ films: data.results })
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  }
+
+  // getHomeWorld = URL => {
+  //   fetch(URL)
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       this.setState({ homeworld: data.results })
+  //     })
+  //     .catch(err => {
+  //       throw new Error(err);
+  //     });
+  // }
+
+  // getSpecies = URL => {
+  //   fetch(URL)
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       this.setState({ species: data.results })
+  //     })
+  //     .catch(err => {
+  //       throw new Error(err);
+  //     });
+  // }
+
+  // getStarShips = URL => {
+  //   fetch(URL)
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       this.setState({ starships: data.results })
+  //     })
+  //     .catch(err => {
+  //       throw new Error(err);
+  //     });
+  // }
+
+
   render() {
+    
+    const {starwarsChars} = this.state;
+    const {films} = this.state
+
+    console.log(starwarsChars, films)
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className='list-container'>{starwarsChars.map(char =>
+          <ul className='list'>
+            <li className='name'><h1>{char.name}</h1></li>
+            <li>Birth Day: {char.birth_year}</li>
+            <li>Gender: {char.gender}</li>
+            <li>Mass: {char.mass}</li>
+            <li>Height: {char.height}</li>
+            <li>Eye color: {char.eye_color}</li>
+            <li>Skin Color: {char.skin_color}</li>
+            <li>Hair Color: {char.hair_color}</li>
+            <li>Created: {char.created}</li>
+            <li>Edited: {char.edited}</li>
+            <li>Films:  </li>
+          </ul>
+        )}</div>
       </div>
     );
   }
